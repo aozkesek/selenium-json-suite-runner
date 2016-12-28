79163cf0fba65825bd0d54406132509292b8047a
Ahmet OZKESEK <aozkesek@gmail.com>
2016-12-28 20:05:06 +0300

    dependency version upgrading, shell script testing are done.

--- hub-server.sh
+++ hub-server.sh
@@ -2,3 +2,4 @@ java -Dselenium.LOGGER=./logs/hub-server.log \
 	-Dselenium.LOGGER.level=ALL \
 	-jar ~/bin/selenium-server-standalone-3.0.1.jar \
 	-role hub \
+	-hubConfig ./hub.json
--- node-server.sh
+++ node-server.sh
@@ -2,7 +2,5 @@ java -Dselenium.LOGGER=./logs/node-server.log \
 	-Dselenium.LOGGER.level=ALL \
 	-jar ~/bin/selenium-server-standalone-3.0.1.jar \
 	-role node \
-	-hubHost localhost \
-	-id node-server-1 \
-	-capabilities browserName=firefox,platform=mac \
+	-nodeConfig	./node.json \
 	
