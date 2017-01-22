55ac3fa051450dedfc081e9237226f3bc0843179
Ahmet OZKESEK <aozkesek@gmail.com>
2017-01-22 22:12:17 +0300

    TestDriver is converted to bean to report aspect requirement

--- src/main/java/org/ao/suite/aspect/ReportAspect.java
+++ src/main/java/org/ao/suite/aspect/ReportAspect.java
@@ -1,6 +1,8 @@
 package org.ao.suite.aspect;
 
+import org.ao.suite.test.TestDriver;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
@@ -21,7 +23,31 @@ public class ReportAspect {
                 System.out.println("report suite.runtests start");    
                 
         }
-              
+        
+        @After("suiteRunTests()")
+        public void reportSuiteRunTestsEnd(JoinPoint jp) {
+                System.out.println("report suite.runtests end");    
+                
+        }
+        
+        @Pointcut("execution(* org.ao.suite.test.TestDriver.run(..))")
+        public void testRun() {}
+
+        @Before("testRun()")
+        public void reportTestRunStart(JoinPoint jp) {
+                System.out.println("report test.run start");    
+                TestDriver td = (TestDriver)jp.getTarget();
+                System.out.println("report test.run " + td.getName());    
+                
+        }
+        
+        @After("testRun()")
+        public void reportTestRunEnd(JoinPoint jp) {
+                System.out.println("report test.run end");    
+                TestDriver td = (TestDriver)jp.getTarget();
+                System.out.println("report test.run " + td.getName());    
+        }
+        
         @Pointcut("execution(* org.ao.suite.test.command.*.execute(..))")
         public void commandExecute() {}
         
