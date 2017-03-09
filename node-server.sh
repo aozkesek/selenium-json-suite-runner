8047ffc5eabfcc7ace2c0ea15de17fa65e86eb92
Ahmet OZKESEK <aozkesek@gmail.com>
2017-03-09 11:38:49 +0300

    selenium version 3.3 upgrade is applied

--- node-server.sh
+++ node-server.sh
@@ -1,6 +1,6 @@
 java -Dselenium.LOGGER=./logs/node-server.log \
 	-Dselenium.LOGGER.level=ALL \
-	-jar ~/bin/selenium-server-standalone-3.0.1.jar \
+	-jar ~/bin/selenium-server-standalone-3.3.0.jar \
 	-role node \
 	-nodeConfig	./node.json \
 	
