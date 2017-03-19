9a3e994da64892025eadb35212792a8b227e3d39
Ahmet OZKESEK <aozkesek@gmail.com>
2017-03-19 12:10:44 +0300

    seleniumversion is upgraded

--- hub-server.sh
+++ hub-server.sh
@@ -1,5 +1,5 @@
 java -Dselenium.LOGGER=./logs/hub-server.log \
 	-Dselenium.LOGGER.level=ALL \
-	-jar ~/bin/selenium-server-standalone-3.3.0.jar \
+	-jar ~/bin/selenium-server-standalone-3.3.1.jar \
 	-role hub \
 	-hubConfig ./hub.json
