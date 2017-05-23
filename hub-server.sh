fdd0b9f93cad0246ce454a8f45752f03bf52ffb3
Ahmet OZKESEK <aozkesek@gmail.com>
2017-05-23 21:18:40 +0300

    selenium standalone server is upgraded

--- hub-server.sh
+++ hub-server.sh
@@ -1,5 +1,5 @@
 java -Dselenium.LOGGER=./logs/hub-server.log \
 	-Dselenium.LOGGER.level=ALL \
-	-jar ~/bin/selenium-server-standalone-3.3.1.jar \
+	-jar ~/bin/selenium-server-standalone-3.4.0.jar \
 	-role hub \
 	-hubConfig ./hub.json
--- standalone-server.sh
+++ standalone-server.sh
@@ -1,4 +1,4 @@
 java -Dselenium.LOGGER=./logs/selenium-server.log \
 	-Dselenium.LOGGER.level=ALL \
-	-jar ~/bin/selenium-server-standalone-3.3.1.jar \
+	-jar ~/bin/selenium-server-standalone-3.4.0.jar \
 	-role standalone \
