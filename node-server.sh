6fab606832582b0bda7edb470ab4ff9198e3b44d
Ahmet OZKESEK <aozkesek@gmail.com>
2017-10-31 11:44:45 +0300

    update dependencies

--- node-server.sh
+++ node-server.sh
@@ -1,6 +1,6 @@
 java -Dselenium.LOGGER=./logs/node-server.log \
 	-Dselenium.LOGGER.level=ALL \
-	-jar ~/bin/selenium-server-standalone-3.4.0.jar \
+	-jar ~/bin/selenium-server-standalone-3.5.3.jar \
 	-role node \
 	-nodeConfig	./node.json \
 	
